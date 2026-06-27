0ef8df3524ae146d6e328ddf43aec9f8025cdbfa
Ricko-22 <rickolek123@gmail.com>
2026-06-27 21:49:22 +0700

    Fix Vite build

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  build: {
-    // CACHING — pisah vendor & app chunk
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 500,
-  },
 })
